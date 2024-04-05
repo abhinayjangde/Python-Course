2500c42f7e0101ab377586a9041d889ec4f57452
Abhinay Jangde <abhinayjangde@gmail.com>
2024-04-05 23:07:02 +0530

    04-Strings in Python

---
+++ 04-Strings in Python/readme.md
@@ -0,0 +1,5 @@
+## Strings
+
+- Strings in python are surrounded by either single quotation marks, or double quotation marks.
+- 'hello' is the same as "hello".
+
---
+++ 04-Strings in Python/strings.py
@@ -0,0 +1,8 @@
+# mystr = """hello, world"""
+# print(mystr)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
