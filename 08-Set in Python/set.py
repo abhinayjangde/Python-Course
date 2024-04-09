34d3999849260fde79976b3d52242d4a1c22dab2
Abhinay Jangde <abhinayjangde@gmail.com>
2024-04-09 16:02:05 +0530

    08-Set in Python

---
+++ 08-Set in Python/set.py
@@ -0,0 +1,20 @@
+myset = {"apple", "banana", "apple"}
+# print(type(myset))
+
+# myset.add("cherry")
+# myset.remove("cherry")
+# myset.pop()
+# print(myset)
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+# print(set1 + set2) # error
+
+# union = set1.union(set2)
+# print(union)
+# set_int = set1.intersection(set2)
+# print(set_int)
+
+# for item in set1:
+#     print(item)
