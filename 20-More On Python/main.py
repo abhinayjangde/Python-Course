f5755d87bf6acf7fc4fac9ff0b5460612463b70e
Abhinay Jangde <abhinayjangde@gmail.com>
2024-07-30 16:03:07 +0530

    all good

---
+++ 20-More On Python/main.py
@@ -0,0 +1,11 @@
+arr = [3,42,90,32,12,4,67,10]
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+print(bubble_sort(arr))
